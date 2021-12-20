2d3e69acf81f23a55621f42b210cbdde1ce4e815
Ricardo Andres Calvo Mendez <rcalvom@unal.edu.co>
2021-12-20 17:13:57 -0500

    Add redux, paper, apollo, envs to package.json

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    'module:metro-react-native-babel-preset'
+  ],
+  plugins: [
+    [
+      "module:react-native-dotenv",
+      {
+        moduleName: "@env",
+        path: ".env",
+      },
+    ],
+  ],
   env: {
     production: {
       plugins: ['react-native-paper/babel'],
--- index.js
+++ index.js
@@ -5,15 +5,24 @@
 import * as React from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider as StoreProvider } from 'react-redux';
+import { ApolloProvider as ApolloProvider } from "@apollo/client";
 import { name as appName } from './app.json';
 import App from './App';
+import { store } from './src/redux/store';
+import client from './src/services/common.services';
+
 
 
 export default function Main() {
     return (
-        <PaperProvider>
-            <App />
-        </PaperProvider>
+        <ApolloProvider client={client}>
+            {/* <StoreProvider store={store}> */}
+            <PaperProvider>
+                <App />
+            </PaperProvider>
+            {/* </StoreProvider> */}
+        </ApolloProvider>
     );
 }
 
