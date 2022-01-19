a1676519d9c6ef8d541a249496985fb17020e3d8
Juan Diego <jdiegopm12@gmail.com>
2022-01-19 18:37:01 -0500

    Fixes

--- src/redux/reducers/index.tsx
+++ src/redux/reducers/index.tsx
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
-import userReducer from './user';
+import { userReducer, currentProfileReducer, profilesReducer } from './user';
 import catalogueReducer from './catalogue';
 import { forum_reducer } from './support';
 
 
 const rootReducer = combineReducers({
     user: userReducer,
+    profile: currentProfileReducer,
+    profiles: profilesReducer,
     catalogue: catalogueReducer,
     forum: forum_reducer
 })
